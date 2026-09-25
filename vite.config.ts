5141eb190dcd3f20bfdafbb1a8d8c0d5e5dd59f4
Cha <87008139+Cha-tries-coding@users.noreply.github.com>
2026-09-25 23:59:29 +0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Crumb-Scene-V1/',
+  base: '/Crumb-Scene/',
+
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
